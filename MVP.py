c6119edf34bcdc7b14f2eff9095a6fa9af9947cc
Rico Taylor <ritayl@my.rongotai.school.nz>
2023-08-04 12:26:06 +1200

    images

--- MVP.py
+++ MVP.py
@@ -13,6 +13,12 @@ toppings = Frame(root, background='#cee741')
 finalise = Frame(root, background='#cee741')
 confirm = Frame(root, background='#cee741')
 
+#images for pizza
+BaseClassic = ImageTk.PhotoImage(file="images/Base_classic.png")
+SauceTomato = ImageTk.PhotoImage(file="images/Sauce_tomato.png")
+CheeseBasic = ImageTk.PhotoImage(file="images/Cheese_basic.png")
+ToppingPepperoni = ImageTk.PhotoImage(file="images/Topping_pepperoni.png")
+
 #making the program open the frames in the full screen.
 root.state("zoomed")
 root.rowconfigure(0, weight=1)
@@ -50,13 +56,21 @@ Button(entry, image=orderLabel, borderwidth=0, bd=5, command=order).pack(pady=0)
 pizza = Frame(base, background='#cee741', height = 550, width = 550)
 pizza.pack(anchor=NE)
 
+#setting up a canvas in order to support png with transparent background
+canvas = Canvas(pizza, bg="#cee741", width=550, height=550)
+canvas.pack()
+
 def putOnPizza(img):
-  label = Label(pizza, image=img)
-  label.place(x=1,y=1)
-
-Button(base, text="Gizza logo", command=lambda:putOnPizza(entryImage)).pack()
-Button(base, text="Order buttton", command=lambda:putOnPizza(orderLabel)).pack()
+  canvas.create_image(275, 275, image=img)
 
 
+Button(base, text="Base", command=lambda:putOnPizza(BaseClassic)).pack()
+Button(base, text="Sauce", command=lambda:putOnPizza(SauceTomato)).pack()
+Button(base, text="Cheese", command=lambda:putOnPizza(CheeseBasic)).pack()
+Button(base, text="Topping", command=lambda:putOnPizza(ToppingPepperoni)).pack()
+
+
+
+#note for next time: I need to make a list, then this list will keep track of what is on the screen at any time. I also need to make a dictionary. This will give each one an attribute for the order that it needs to go. Then when another option is picked it can be swapped out for the thing in the list with the same attribute/order number.
 
 root.mainloop()
